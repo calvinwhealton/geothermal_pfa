01c78b4a399206c3f367a82c0fa889e5141ecefb
Calvin Whealton <caw324@cornell.edu>
2015-09-01 16:16:37 -0400

    made code more versatile
    made code more versatile to include logarithmic parameters, reversed
    axis (small is good)

--- combining_metrics/convertRasterPFAMetric.R
+++ combining_metrics/convertRasterPFAMetric.R
@@ -3,34 +3,66 @@
 #     rast        = raster object with values
 #     thresholds  = play fairway thresholds with [min, threshold1,...,threshold2/4, max]
 #     ignore      = numerical value to ignore
+#     log_scale   = whether conversion should be done on a log-scale (thresholds and values on real-scale)
+#     rev_scale   = whether the scale should be reversed (low is good, high is bad)
 
-convRastPFRank <- function(rast         # raster
-                           ,thresholds  # thresholds including min and max values
-                           ,ignore = -9999
+convRastPFRank <- function(rast               # raster
+                           ,thresholds        # thresholds including min and max values
+                           ,ignore = -9999    # values to ignore
+                           ,log_scale=FALSE   # whether to convert using log scale
+                           ,rev_scale=FALSE   # whether scale is reversed
                            ){
   
-  # initializing raster as the original value
-  rast_pfa <- rast
+  # initializing raster and thresholds
+  if(log_scale == TRUE){ # case for when log-scale
+    
+    # initialize raster/thresholds as log10 of input raster/thresholds
+    rast_pfa <- calc(rast,fun=function(x){log10(x)}) 
+    thresh <- log10(thresholds)
+  
+  }else if(log_scale == FALSE){ # case for not log-scale
+    
+    # initialize raster/thresholds as input raster/thresholds
+    rast_pfa <- rast
+    thresh <- thresholds
+  
+  }else{ # error message
+    
+    print('Not a valid selection for log_scale')
+  }
   
   # setting number scheme based on number of thresholds
-  num_cat <- seq(0,length(thresholds)-1,1)
+  # thresh includes min, max, and category separation values
+  num_cat <- seq(0,length(thresh)-1,1)
   
   # assigning 0 to values < minimum criteria
   # only assigning values with raster cell is not ignored
-  rast_pfa[(rast != ignore)&(rast < thresholds[1])] <- num_cat[1]
+  rast_pfa[(rast != ignore)&(rast < thresh[1])] <- num_cat[1]
   
   # assigning highest to values >= maximum criteria
   # only assigning values with raster cell is not ignored
-  rast_pfa[(rast != ignore)&(rast >= thresholds[length(thresholds)])] <- num_cat[length(num_cat)]
+  rast_pfa[(rast != ignore)&(rast >= thresh[length(thresh)])] <- num_cat[length(num_cat)]
   
   # looping over intermediate values (between min and max)
-  for(i in 1:(length(thresholds)-1)){
+  for(i in 1:length(num_cat)){
     
     # values between the two thresholds are linearly scaled
-    rast_pfa[(rast >= thresholds[i])&(rast < thresholds[i+1])] <- (rast[(rast >= thresholds[i])&(rast < thresholds[i+1])] - thresholds[i])/(thresholds[i+1]-thresholds[i]) + i - 1
-
+    rast_pfa[(rast >= thresh[i])&(rast < thresh[i+1])] <- (rast[(rast >= thresh[i])&(rast < thresh[i+1])] - thresh[i])/(thresh[i+1]-thresh[i]) + i - 1
   }
   
   # returning calculated raster
-  return(rast_pfa)
+  if(rev_scale == TRUE){ # case for reversing the scale
+    
+    # reversing scale by subtracting raster from the maximum value
+    return(calc(rast_pfa,fun=fun(x){num_cat[length(num_cat)]-x}))
+    
+  }else if(rev_scale == FALSE){ # case for scale not being reversed
+    
+    # no adjustment of the calculated raster
+    return(rast_pfa)
+    
+  }else{ # error message
+    
+    print('Not a valid selection for rev_scale')
+  }
 }
