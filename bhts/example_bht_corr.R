3f03adf6bcdf5758edfe0144cd51dcf13030ac12
Calvin Whealton <caw324@cornell.edu>
2015-09-03 10:59:56 -0400

    updated sample script for BHT corrections

--- bhts/example_bht_corr.R
+++ bhts/example_bht_corr.R
@@ -12,8 +12,12 @@ source("func_BHT_NY_PA_WV_corr.R")
 test_data <- read.table('bht_test_data.csv', header=TRUE, sep=',')
 
 # running function
-corrected_BHTS <- NY_PA_BHT2(test_data)
+corrected_BHTS <- NY_PA_WV_BHT2(test_data)
 
-plot(corrected_BHTS$calc_depth_m[which(corrected_BHTS$calc_depth_m < 10000)]
-     , corrected_BHTS$corr_bht_c[which(corrected_BHTS$calc_depth_m < 10000)]- corrected_BHTS$bht_c[which(corrected_BHTS$calc_depth_m < 10000)]
-     , pch = corrected_BHTS$reg[which(corrected_BHTS$calc_depth_m < 10000)]+15)
+corrected_BHTS$bht_corr <- corrected_BHTS$corr_bht_c - corrected_BHTS$bht_c
+
+
+plot(corrected_BHTS$calc_depth_m[corrected_BHTS$calc_depth_m<6000]
+     , corrected_BHTS$corr_bht_c[corrected_BHTS$calc_depth_m<6000]-corrected_BHTS$bht_c[corrected_BHTS$calc_depth_m<6000]
+     , pch = corrected_BHTS$reg[corrected_BHTS$calc_depth_m<6000]+15)
+grid(lwd=3)
