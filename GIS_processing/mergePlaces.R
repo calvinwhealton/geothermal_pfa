917d0cd5f7db90591607c2d9e33573e954602383
Calvin Whealton <caw324@cornell.edu>
2016-05-16 14:01:22 -0400

    corrected issues with indexing for merged locations

--- GIS_processing/mergePlaces.R
+++ GIS_processing/mergePlaces.R
@@ -4,9 +4,13 @@
 # 1. loading libraries
 # 2. loading US Census places shapefile
 # 3. determining which places are candidates for merging
-# 4. mering values
+# 4. mering places (sum population and area)
 # 5. repeating 3 and 4 until no more merges are needed
 # 6. saving merged shapefile
+
+# at the moment the places can only be merged once
+# a place can be merged with its neighboring places
+# but not the neighbors of the neighbors
 
 # loading libraries
 library(sp)           # for transforming coordinates
@@ -15,12 +19,6 @@ library(rgdal)        # reading in data, readOGR() and writeOGR()
 library(spdep)        # other spatial processing
 library(rgeos)        # for buffering places of interest and finding intersections
 library(maptools)
-library()
-#library(xlsx)         # for reading in xlsx tables
-#library(RColorBrewer) # R color brewer palettes for parallel axis plot
-#library(pracma)       # for interpolation in tables
-#library(vioplot)      # for violin plots
-
 
 # reading-in shapefile with US Census "places" clipped to states and region
 setwd('/Users/calvinwhealton/Documents/GIS Projects/COSUNA_Paper/cb_2015_us_state_500k_copy_clipAppSt')
@@ -116,7 +114,7 @@ placesTransTemp <- placesTrans
 placesTransTemp$mergeID <- NA
 placesTransTemp$name2 <- placesTransTemp$NAMELSAD10
 
-# distance for buffering
+# distance for buffering (meters since in UTM)
 BufDist <- 50
 
 # buffering polygons
@@ -125,39 +123,43 @@ placesTransBufTemp <- gBuffer(placesTrans # shapefile
                             ,width=BufDist # in units of projection
 )
 
-placesTransContTemp <- gIntersects(placesTransBufTemp,byid = T)
+placesTransContTemp <- gIntersects(placesTransBufTemp
+                                   ,byid = T)
 
-# goal for population, will not merge areas above this population
+# goal for population, will not merge places above this population to other places above this population
+# will merge places above this population to places below this population
 popGoal <- 10000
 
 # creating a sorted vector for population
 popCheck <- placesTrans[['DP0010001']]
+areaLandCheck <- placesTrans[['ALAND10']]
+areaWaterCheck <- placesTrans[['AWATER10']]
+houseHoldsCheck <- placesTrans[['DP0130001']]
 
 # creating list with the population sorted (smallest to largest)
 # and the index of the value
-popCheckSort <- as.data.frame(cbind(seq(1,length(placesTrans),1),placesTrans[['DP0010001']]))
-names(popCheckSort) <- c("ind","pop")
+popCheckSort <- as.data.frame(cbind(seq(1,length(placesTrans),1),popCheck,areaLandCheck,areaWaterCheck,houseHoldsCheck))
+names(popCheckSort) <- c("ind","pop","areaLand","areaWater","houseHolds")
 popCheckSort$mergeID <- NA # storing the ID
-popCheckSort$mergPop <- NA # storing merged population
+#popCheckSort$mergPop <- NA # storing merged population
 popCheckSort$neighbors <- apply(placesTransContTemp == T,2,sum) # number of neighbors
 popCheckSort$numMerges <- 0
 popCheckSort$numNeighborsMerged <- NA
 
-# creating list with the population sorted (smallest to largest)
-# and the index of the value
+# creating a temporary set of values to use in calculations
 popCheckSortTemp <- popCheckSort
 
 # variable to control while loop
 merging <- T
 
-popCheckSortTemp2 <- popCheckSort
-popCheckSortTemp2 <- popCheckSortTemp2[order(popCheckSortTemp2$pop),]
-
+# popCheckSortTemp2 <- popCheckSort
+# popCheckSortTemp2 <- popCheckSortTemp2[order(popCheckSortTemp2$pop),]
 
 # loop for merging
 while(merging){
   
   # creating ordered data frame from smallest population to largest
+  # NAs will be at the bottom of the list
   popCheckSortTemp <- popCheckSortTemp[order(popCheckSortTemp$pop),]
   
   # variable controlling looping
@@ -169,199 +171,107 @@ while(merging){
   # looping through the data set until merged or merging not needed
   while(looping){
     
-    # if population is less than the population goal
-    if(popCheckSortTemp$pop[counter] < popGoal){
+    # checking if it has not been merged and if population is less than popGoal
+    if((is.na(popCheckSortTemp$mergeID[counter])) & (popCheckSortTemp$pop[counter] < popGoal)){
       
-      # condition of no neighbors
+      # checking for no neighbors
       if(popCheckSortTemp$neighbors[counter] == 1){
         
         # no neighbors
-        # population stays the same
-        popCheckSortTemp$mergeID[counter] <- -9999
+        # population and other variables stays the same
+        popCheckSortTemp$mergeID[counter] <-  popCheckSortTemp$ind[counter]
         
         # indexing counter
         counter <- counter + 1
         
         # continue to loop
         looping <- T
-        print("hi-1")
-      }else{
+      
+      }else{ # has neighbors
         
-        # indices of neighbors
-        # checking if values were already merged
-        if(popCheckSortTemp$numMerges[counter] == 0){
-          
-          # indices that are marked as neighbors (first-order neighbors, touching)
-          indsNeighbors <- which(placesTransContTemp[,popCheckSortTemp$ind[counter]] ==T)
+        # indices that are marked as neighbors (first-order neighbors, touching)
+        indsNeighbors <- which(placesTransContTemp[,popCheckSortTemp$ind[counter]] ==T)
         
-          # should calculations be continued
-          continueCalcs <- T
-          
-#         }else if(popCheckSortTemp$numMerges[counter] == 1){
-#           
-#           # indices that are marked as second-order neighbors (touching a neighbor)
-#           indsNeighborFirst <- which(placesTransContTemp[,popCheckSortTemp$ind[counter]] ==T)
-#           
-#           # vector of neighbors
-#           indsNeighbors <- NULL
-#           
-#           for(i in indsNeighborFirst){
-#             
-#             # finding the second order connections
-#             indsNeighbors <- c(indsNeighbors,which(placesTransContTemp[,i] ==T))
-#             
-#           }
-#           
-#           # should calculations be continued
-#           continueCalcs <- T
-#         
-        }else{
-          print("hi0")
-          # should calculations be continued
-          # not continuing calculations beyond 2 merges
-          continueCalcs <- F
-          popCheckSortTemp$mergeID[counter] <- -1111
+        # index of place
+        indPlace <- popCheckSortTemp$ind[counter]
+        
+        # index of place sorted
+        indPlaceSorted <- which(popCheckSortTemp$ind == indPlace)
+        
+        # finding row indices in sorted dataframe
+        indsNeighborsSorted <- NULL
+        
+        # finding the sorted data frame row indices
+        for(i in indsNeighbors){
+          indsNeighborsSorted <- c(indsNeighborsSorted,which(popCheckSortTemp$ind == i))
         }
         
-        if(continueCalcs){
-          
-          # index of place
-          indPlace <- popCheckSortTemp$ind[counter]
-          
-          # summing the population
-          # finding indices in sorted matrix
-          indsNeighborsSorted <- NULL
-          for(i in indsNeighbors){
-            indsNeighborsSorted <- c(indsNeighborsSorted,which(popCheckSortTemp$ind == i))
-          }
+        # removing places that were already merged from the neighbors
+        indsNeighborsSortedNoNA <- indsNeighborsSorted[which(is.na(popCheckSortTemp$pop[indsNeighborsSorted]) == F)]
         
-          # adding the population
-          popCheckSortTemp$pop[counter] <- sum(popCheckSortTemp$pop[indsNeighborsSorted],na.rm=T)
-          
-          # filling in NA for places that were merged
-          for(i in setdiff(indsNeighbors,indPlace)){
-            ind <- which(popCheckSortTemp$ind %in% i)
-            popCheckSortTemp$pop[ind] <- NA
-          }
-          
-          
-          popCheckSortTemp$numMerges[counter] <- popCheckSortTemp$numMerges[counter] +1
-          
-          popCheckSortTemp$numNeighborsMerged[counter] <- length(indsNeighborsSorted)-1
-          
-          popCheckSortTemp$mergeID[indsNeighborsSorted] <- popCheckSortTemp$ind[counter] 
-          print("hi1")
-          # stop looping because of merges
-          looping <- F
-        }else{
-          print("hi2")
-          # continue looping
-          # because of no merges
-          looping <- T
-          
-          counter <- counter + 1
-        }
+        # adding the population and other variables to the existing ones
+        popCheckSortTemp$pop[counter] <- sum(popCheckSortTemp$pop[indsNeighborsSortedNoNA])
+        popCheckSortTemp$areaLand[counter] <- sum(popCheckSortTemp$areaLand[indsNeighborsSortedNoNA])
+        popCheckSortTemp$areaWater[counter] <- sum(popCheckSortTemp$areaWater[indsNeighborsSortedNoNA])
+        popCheckSortTemp$houseHolds[counter] <- sum(popCheckSortTemp$houseHolds[indsNeighborsSortedNoNA])
+        
+        # assigning mergeID
+        popCheckSortTemp$mergeID[indsNeighborsSortedNoNA] <- popCheckSortTemp$ind[counter]
+        
+        # counting the number of neighbors merged
+        popCheckSortTemp$numNeighborsMerged[counter] <- length(indsNeighborsSortedNoNA)-1
+        
+        # filling in NA for values
+        popCheckSortTemp$pop[setdiff(indsNeighborsSortedNoNA,indPlaceSorted)] <- NA
+        popCheckSortTemp$areaLand[setdiff(indsNeighborsSortedNoNA,indPlaceSorted)] <- NA
+        popCheckSortTemp$areaWater[setdiff(indsNeighborsSortedNoNA,indPlaceSorted)] <- NA
+        popCheckSortTemp$houseHolds[setdiff(indsNeighborsSortedNoNA,indPlaceSorted)] <- NA
+        
+        # indexing the number of merges for the place
+        popCheckSortTemp$numMerges[indsNeighborsSortedNoNA] <- popCheckSortTemp$numMerges[indsNeighborsSortedNoNA] +1
+    
+        # stop looping because of merges
+        looping <- F
       }
+    }else{
+      
+      # continuing to loop
+      looping <- T
+      
+      # incrementing counter
+      counter <- counter + 1
     }
   }
 
- 
   print(popCheckSortTemp$pop[counter])
-  #print(head(popCheckSortTemp))
-  
-  # condition to end the loop
+
+    # condition to end the loop
   # next population has value over population goal
+  # must use counter + 1 instead of counter otherwise
+  # loop will stop if the merged population is >= popGoal
   if(popCheckSortTemp$pop[counter+1] >= popGoal){
-    print("hi3")
     merging <- F
   }
 }
 
+# assigning the merge ID to the places that did not merge and pop > popGoal
+indsHighPop <- which(popCheckSortTemp$mergeID %in% NA)
+popCheckSortTemp$mergeID[indsHighPop] <- popCheckSortTemp$ind[indsHighPop]
 
-placesTrans$NAMELSAD10[c(1132,969,590,959,1669,2408,7,1935,15,1172,1671,799)]
+# merging values
+# assigning values to the placesTrans variable
+popCheckSortTemp <- popCheckSortTemp[order(popCheckSortTemp$ind),]
+placesTrans$merger <- popCheckSortTemp$mergeID
+placesTrans$pop <- popCheckSortTemp$pop[popCheckSortTemp$mergeID]
+placesTrans$areaLand <- popCheckSortTemp$areaLand[popCheckSortTemp$mergeID]
+placesTrans$areaWater <- popCheckSortTemp$areaWater[popCheckSortTemp$mergeID]
+placesTrans$houseHolds <- popCheckSortTemp$houseHolds[popCheckSortTemp$mergeID]
 
+writeOGR(placesTrans,dsn="placesTrans",layer="placesTrans5",driver="ESRI Shapefile")
 
-
-
-
-
-
-
-# loop over all places
-for(i in 1:nrow(popCheckSort)){
-  
-  # condition if population is less than the population goal
-  if(popCheckSort$pop[i] < popGoal){
-    
-    # no neighbors to merge with
-    if(sum(placesTransContTemp[,popCheckSort$ind[i]]==T) == 1){
-      
-      popCheckSortTemp
-    }
-    
-  }else{
-    
-    popCheckSortTemp$mergeID[i] <- placesTrans$GEOID10[popCheckSort$ind[i]]
-    
-  }
-  
-}
+placesTransUnion <- unionSpatialPolygons(placesTrans, placesTrans$merger)
 
 
-
-# creating a sorted vector for population
-popCheck <- placesTrans[['DP0010001']]
-
-# creating list with the population sorted (smallest to largest)
-# and the index of the value
-popCheckSort <- cbind(sort.list(popCheck),placesTrans[['DP0010001']][sort.list(popCheck)])
-
-mergeIndicator <- rep(NA,nrow(popCheckSort))
-
-
-
-
-
-
-
-
-
-# finding neighborhood based on distance
-# minimum distance = d1 = 0 m
-# maximum distance = d2 = 5000 m
-# distance units in same units as projection
-placesNeigh = dnearneigh(coordinates(placesTrans),d1=0,d2=5000)
-
-# creating a sorted vector for population
-popCheck <- placesTrans[['DP0010001']]
-
-# creating list with the population sorted (smallest to largest)
-# and the index of the value
-popCheckSort <- cbind(sort.list(popCheck),placesTrans[['DP0010001']][sort.list(popCheck)])
-
-mergeIndicator <- rep(NA,nrow(popCheckSort))
-
-# lower limit for stopping merging
-lowLimMerge <- 4000
-
-# looping through to determine if things should be merged
-for(i in 1:nrow(popCheckSort)){
-  
-  # -9999 if population is above value
-  if(popCheckSort[i,2] > lowLimMerge){
-    mergeIndicator[i] <- -9999
-  }else{
-    if(length(placesNeigh[[popCheckSort[i,1]]]) == 1){
-      if(placesNeigh[[popCheckSort[i,1]]] == 0){
-        mergeIndicator[i] <- -1111
-      }else{
-        mergeIndicator[i] <- popCheckSort[i,2]+placesTrans[['DP0010001']][placesNeigh[[popCheckSort[i,1]]]]
-      }
-    }else{
-      mergeIndicator[i] <- popCheckSort[i,2]+placesTrans[['DP0010001']][placesNeigh[[popCheckSort[i,1]]]]
-    }
-  }
-}
 
 
 indsPlot <- which(mergeIndicator > 0)
